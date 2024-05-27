5f7d4fd7a91d8783c84a7150dbb9a067552e7903
Abraham <abukip344@gmail.com>
2024-05-27 00:47:23 -0400

    anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,28 @@
 # your code goes here!
+
+class Anagram:
+    '''modelling an anagram'''
+    def __init__(self, word):
+        '''initialize attributes'''
+        self.word = word
+
+    def match(self, possible_anagrams):
+        '''matching anagrams'''
+        # matching_anagram = []
+        # for anag in possible_anagrams:
+        #     if sorted([char for char in anag]) == sorted([chara for chara in self.word]):
+        #         #matching_anagram.append[anag]
+        #         matching_anagram.append(anag)
+        #         print(anag)
+        #     else:
+        #         print([])
+        # #return matching_anagram
+        match = [anag for anag in possible_anagrams if sorted([char for char in anag]) == sorted([chara for chara in self.word])]
+        return match
+    #print(matching_anagram)
+
+
+my_word = Anagram("listen")
+
+my_word.match(['enlists', 'google', 'inlets', 'banana'])
+
